c5b9680e28825fe542dfddf5e7c3b998e08b22e5
Steve Hajducko <steven_hajducko@intuit.com>
2017-01-17 17:09:21 -0800

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyqb',
-      version='0.2.0',
+      version='0.2.1',
       description='Quickbase API Python Wrapper',
       url='http://github.com/sjmh/pyqb',
       author='Steven Hajducko',
