44b447feffb24a8847e74820e1b033c6ab763cff
Steve Hajducko <hajducko@gmail.com>
2019-10-30 22:11:40 -0700

    Bump to 0.2.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyqb',
-      version='0.2.3',
+      version='0.2.4',
       description='Quickbase API Python Wrapper',
       url='http://github.com/sjmh/pyqb',
       author='Steven Hajducko',
